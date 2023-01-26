fd1f6148e9b3c5071a16390acaefd461fb83df17
Benjamin Perraudin <bperraud@student.s19.be>
2023-01-26 14:20:52 +0100

    non writable my.cnf

--- srcs/requirements/mariadb/setup_db.sh
+++ srcs/requirements/mariadb/setup_db.sh
@@ -6,16 +6,22 @@ mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 # Start the MariaDB service
 mariadbd &
 
-# Wait for MySQL to start
+# Wait for MariaDB to start
 sleep 5
 
+# Set a new password for the root user
+mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('new_password');"
+
 # Create new user and grant privileges to WordPress database
-mysql -u root -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';"
+mariadb -u root -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'password';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';"
 
 # Create administrator user and grant privileges to WordPress database
-mysql -u root -e "CREATE USER 'adminuser'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'adminuser'"
+mariadb -u root -e "CREATE USER 'adminuser'@'localhost' IDENTIFIED BY 'password';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'adminuser'@'localhost';"
+
+# Reload the grant tables to activate the new changes
+mariadb -u root -pnew_password -e "FLUSH PRIVILEGES;"
 
 # Stop the MariaDB service
 mariadb-admin -u root shutdown
