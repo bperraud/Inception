a77a9aa0e7d10807f3d3dedbcaf3d47267543363
Benjamin Perraudin <bperraud@student.s19.be>
2023-02-02 02:33:38 +0100

    wp core download

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 
+wp core download
 
 # If wp-config doesnt exist, creates it
 if [ ! -f /usr/share/html/wp-config.php ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/bin/wp
+	wp core download --allow-root
+
 	wp config create --dbname=$WP_DB --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=mariadb --allow-root
-	#mv wp-config.php /usr/share/html/
 fi
 
 # Execute any command line arguments passed to the container
