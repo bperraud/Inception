a611b10a278adf3f09b4042cd9c36d6c23590842
Benjamin Perraudin <bperraud@student.s19.be>
2023-02-02 01:59:52 +0100

    wp-config.php creation good

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# Move the files from /tmp/wordpress/ to /usr/share/html/
-#mv /tmp/wordpress* /usr/share/html/
 
-if [ ! -f /usr/share/html/wordpress/wp-config.php ]; then
+
+# If wp-config doesnt exist, creates it
+if [ ! -f /usr/share/html/wp-config.php ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/bin/wp
 	wp config create --dbname=$WP_DB --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=mariadb --allow-root
-
-	mv wp-config.php /usr/share/html/wordpress/
-
+	#mv wp-config.php /usr/share/html/
 fi
 
 # Execute any command line arguments passed to the container
