01940bd9a61fd9cfc6c4b8ea7d815a39a24e164a
Benjamin Perraudin <bperraud@mi-c2-p7.s19.be>
2023-02-14 16:28:18 +0100

    user vm

--- setupVM.sh
+++ setupVM.sh
@@ -16,5 +16,6 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt-get install docker-compose
-sudo usermod -aG docker lubuntu
+sudo groupadd docker
+sudo usermod -aG docker $USER
 sudo service docker restart
