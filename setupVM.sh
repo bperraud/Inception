1adc3c021c43d706893f361ceb3369ae2b6cfb7a
Benjamin Perraudin <bperraud@fu-r5-p7.s19.be>
2023-02-14 17:30:41 +0100

    no groupadd

--- setupVM.sh
+++ setupVM.sh
@@ -16,6 +16,5 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt-get install docker-compose
-sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker restart
