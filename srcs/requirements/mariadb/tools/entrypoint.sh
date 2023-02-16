4642757e452ad1bfb9ec6d0b16876de40cb9617f
Benjamin Perraudin <bperraud@fu-c3-p6.s19.be>
2023-02-16 18:22:20 +0100

    condition db

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,9 +3,9 @@
 envsubst < /tools/init.sql | sponge /tools/init.sql
 
 if [ -d "/var/lib/mysql/my_database" ]; then
-	echo "$@"
-	echo "$1"
-	exec "$@"
+	echo $@
+	echo $1
+	exec $@
 else
-	exec "$@"
+	exec $@
 fi
